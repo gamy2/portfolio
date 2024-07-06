6b9ffa012031fbe1669ab9c2d36eb8f6997e7efc
mohamed gamal <76060169+gamy2@users.noreply.github.com>
2024-07-06 06:46:03 +0300

    adding clsx befor using aceternity ui

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="pb-20 pt-36">Hero</div>;
+  return (
+    <div className="pb-20 pt-36">
+      <div>Hero</div>
+    </div>
+  );
 };
 
 export default Hero;
