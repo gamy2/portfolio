d3cf1cf68cd8904cdbfda976145fc7e789f8606f
mohamed gamal <76060169+gamy2@users.noreply.github.com>
2024-07-06 07:11:54 +0300

    adding dots and spotligh
    adding dots and ues it as background  then add spotligh  to hero seaction
    
    change every thing to bg-black-100

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { Spotlight } from "./ui/Spotlight";
+import { DotBackgroundDemo } from "./ui/background";
+
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div>Hero</div>
+      {/* <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" /> */}
+      <DotBackgroundDemo />
     </div>
   );
 };
