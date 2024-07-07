ccdf511fce94dbd5c535ddf3ef82b953a0554144
mohamed gamal <76060169+gamy2@users.noreply.github.com>
2024-07-07 08:20:21 +0300

    adding ainmation text
    adding ainmation text using aceternity ui and handle how we can make color change in text generator using ternary operator

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-import { Spotlight } from "./ui/Spotlight";
+import { Spotlight } from "./ui/spotlight";
 import { DotBackgroundDemo } from "./ui/background";
+import { TextGenerateEffect } from "./ui/text_generate";
 
 const Hero = () => {
   return (
@@ -11,8 +12,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="text-blue-100 uppercase text-xs text-center max-w-80">
-            Hello Next.js
+            Using Next.js
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl capitalize"
+            words="transforming your dreams and ideas into Seamless websites"
+          />
+          <p className="text-center mb-4 capitalize md:tracking-wider text-sm md:text-lg lg:text-2xl">
+            Hi I&apos;m Mohamed Gamal, a Front-End and Cross-Platform Developer
+          </p>
         </div>
       </div>
     </div>
