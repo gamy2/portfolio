37dca3a914445cfd3f0e10e2cd45771c9aaab27d
mohamed gamal <76060169+gamy2@users.noreply.github.com>
2024-07-18 19:38:54 +0300

    start handling images

--- components/ui/bento_grid.tsx
+++ components/ui/bento_grid.tsx
@@ -25,8 +25,6 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
   id,
   img,
   imgClassName,
@@ -36,8 +34,6 @@ export const BentoGridItem = ({
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
   id?: number;
   img?: string;
   imgClassName: string;
@@ -50,10 +46,24 @@ export const BentoGridItem = ({
         "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: "rgb(6,4,33)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(6,4,33,1) 0%, rgba(7,5,85,1) 100%)",
+      }}
     >
-      {header}
+      <div className={`'flex justify-center h-full'`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center")}
+            />
+          )}
+        </div>
+      </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
